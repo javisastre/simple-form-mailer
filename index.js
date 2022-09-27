476a3e5a5a5a597d6f6b6ce8b847b189f6ed8e6c
Javi Sastre <j.sastre@cadt.com>
2022-09-27 11:28:12 +0200

    updated code

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const nodemailer = require("nodemailer");
-require("dotenv").config();
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
 const route = express.Router();
 
 const port = process.env.PORT || 5000;
@@ -18,58 +18,30 @@ app.listen(port, () => {
 });
 
 const transporter = nodemailer.createTransport({
-  port: 465,
-  host: "smtp.gmail.com",
-  auth: {
-    user: process.env.EMAIL,
-    pass: process.env.PASSWORD,
-  },
-  secure: true,
+  service: "gmail",
+  providerauth: { user: process.env.EMAIL },
+  pass: process.env.PASSWORD,
 });
 
 route.post("/contact", (req, res) => {
   const { to, subject, text } = req.body;
+
   const mailData = {
-    from: "j.sastre@cadt.com",
-    to: to || "j.sastre@cadt.com",
-    subject: subject || "no subject",
-    text: text || "no message",
+    from: process.env.EMAIL,
+    to: to || "test@email.com",
+    subject: subject || "[no subject]",
+    text: text || "[no email text]",
+    // attachments: [
+    //   {
+    //     filename: "text notes.txt",
+    //     path: "notes.txt",
+    //   },
+    // ],
   };
 
-  transporter.sendMail(mailData, (error, info) => {
-    if (error) {
-      return console.log(error);
-    }
-    res.status(200).send({ message: "Mail send", message_id: info.messageId });
+  transporter.sendMail(mailData, (err, info) => {
+    if (err) console.log(err);
+
+    res.status(200).send({ message: "Mail send", info });
   });
 });
-
-// route.post("/attachments-mail", (req, res) => {
-//   const { to, subject, text } = req.body;
-//   const mailData = {
-//     from: "youremail@gmail.com",
-//     to: to,
-//     subject: subject,
-//     text: text,
-//     html: "<b>Hey there! </b><br> This is our first message sent with Nodemailer<br/>",
-//     attachments: [
-//       {
-//         // file on disk as an attachment
-//         filename: "nodemailer.png",
-//         path: "nodemailer.png",
-//       },
-//       {
-//         // file on disk as an attachment
-//         filename: "text_file.txt",
-//         path: "text_file.txt",
-//       },
-//     ],
-//   };
-
-//   transporter.sendMail(mailData, (error, info) => {
-//     if (error) {
-//       return console.log(error);
-//     }
-//     res.status(200).send({ message: "Mail send", message_id: info.messageId });
-//   });
-// });
